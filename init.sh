acbca599378fc5a9316883c9f8411a4edb75fa49
iqiziqi <iqiziqi@outlook.com>
2020-02-17 22:10:37 +0800

    [udpate] add autosuggestions plugin for zsh

--- init.sh
+++ init.sh
@@ -2,5 +2,20 @@
 
 root=$(cd `dirname $0`; pwd)
 
+function install_zsh_plugin() {
+  name=`basename $1`
+  plugin_path="${ZSH_CUSTOM:-"$HOME/.oh-my-zsh/custom"}/plugins/$name"
+  echo $plugin_path
+  if [ ! -d "$plugin_path" ]; then
+    echo "install oh-my-zsh plugin $name"
+    git clone $1 $plugin_path
+    echo "complete install oh-my-zsh plugin $name!!"
+  else
+    echo 'fuck'
+  fi
+}
+
+install_zsh_plugin 'https://github.com/zsh-users/zsh-autosuggestions'
+
 ln -sf "$root/zsh/.zshrc" "$HOME/.zshrc"
 ln -sf "$root/zsh/custom.zsh-theme" "$HOME/.oh-my-zsh/themes/custom.zsh-theme"
