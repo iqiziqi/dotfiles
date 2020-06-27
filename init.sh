43d35f5195b8768cbb36fa6219d4295ffa4c336d
work <work@192.168.168.239>
2020-06-28 02:51:39 +0800

    [update] renew init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,14 @@
 
 root=$(cd `dirname $0`; pwd)
 
+function install_oh_my_zsh() {
+  if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh)"
+  else
+    echo 'oh-my-zsh is installed'
+  fi
+}
+
 function install_zsh_plugin() {
   name=`basename $1`
   plugin_path="${ZSH_CUSTOM:-"$HOME/.oh-my-zsh/custom"}/plugins/$name"
@@ -11,10 +19,12 @@ function install_zsh_plugin() {
     git clone $1 $plugin_path
     echo "complete install oh-my-zsh plugin $name!!"
   else
-    echo 'fuck'
+    echo "plugin $name is installed"
   fi
 }
 
+# install oh-my-zsh
+install_oh_my_zsh
 install_zsh_plugin 'https://github.com/zsh-users/zsh-autosuggestions'
 
 ln -sf "$root/zsh/.zshrc" "$HOME/.zshrc"
